0c4f899bc78adf2905ccbf26f66c114e5b1d23b4
SarahGormley <sarah.gormley39@gmail.com>
2022-06-05 15:58:22 +1000

    fetch request working. Data populating on page.

--- script.js
+++ script.js
@@ -1,33 +1,76 @@
 var apiKey = "74b589766e2c1c1f69016c5e5740ff80";
 var searchBtn = document.getElementById("search-btn");
-
-var cityName = document.getElementById("city-name");
+var cityName = document.getElementById("search-text");
+var searchCityName = document.getElementById("search-city-name");
 var temperature = document.getElementById("temperature");
 var wind = document.getElementById("wind");
 var humidity = document.getElementById("humidity");
 var currentUVIndex = document.getElementById("uvIndex");
-var weatherIcon = document.getElementById("weather-icon")
-var city;
+var weatherIcon = document.getElementById("weather-icon");
+var currentDay = document.getElementById("current-day");
+
 var currentDate = moment().format("MMM Do, YYYY");
 
-
-
-function handleFormSubmit(event) { //Change the parameter to represent the event object that will be passed
-    var cityName = document.querySelector("#search-text").value; //change form ==> document so you can select the text value
-    getCity(cityName);
+function pageinit() {
+    getPrevCities()
 }
+searchBtn.addEventListener('click', function() {
+    getCity(cityName)
+})
 
 // function to get the selected city from the search bar
 
 function getCity(cityName) {
-    let requestURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
+    let requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName.value + "&units=metric&appid=" + apiKey;
     fetch(requestURL)
         .then(function(response) {
-            console.log(requestURL)
+            response.json().then(function(data) {
+                console.log(data);
+                displayCityData(data);
+            })
         });
 
+};
+
+function displayCityData(data) {
+    var searchedCityName = data.name;
+    var cityTemp = data.main.temp;
+    var windSpeed = data.wind.speed;
+    var searchedHumidity = data.main.humidity;
+    var latitude = data.coord.lat;
+    var longitude = data.coord.lon;
+
+    searchCityName.innerHTML = searchedCityName;
+    temperature.innerHTML = cityTemp + "°C";
+    wind.innerHTML = windSpeed + "km/h";
+    humidity.innerHTML = searchedHumidity + "%";
+    currentDay.innerHTML = currentDate;
+
+    var currentWeatherIcon = document.createElement("img");
+    currentWeatherIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+    weatherIcon.appendChild(currentWeatherIcon);
+
+
+    getUVIndex(latitude, longitude);
 }
 
+function getUVIndex(latitude, longitude) {
+    console.log(latitude, longitude)
+    let uvURL = "https://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + latitude + "&lon=" + longitude;
+    fetch(uvURL)
+        .then(function(index) {
+            index.json().then(function(data) {
+                console.log(data)
+                if (data.value < 4) {
+                    currentUVIndex.setAttribute("id", "green-UV");
+                } else if (data.value < 8) {
+                    currentUVIndex.setAttribute("id", "yellow-UV");
+                } else {
+                    currentUVIndex.setAttribute("id", "red-UV");
+                }
 
+                currentUVIndex.innerHTML = data.value;
 
-searchBtn.addEventListener("click", handleFormSubmit)
+            })
+        })
+}
